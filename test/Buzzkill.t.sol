4dac8d4c7ef267c291770e3930e494469a068d4f
ptaku35 <phoenixtaku9@gmail.com>
2023-11-27 22:31:01 +0700

    Add test

--- test/Buzzkill.t.sol
+++ test/Buzzkill.t.sol
@@ -1,11 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "lib/forge-std/Test.sol";
-
+import {Test} from "lib/forge-std/src/Test.sol";
+import {Buzzkill} from "../src/Buzzkill.sol";
 
 contract BuzzkillTest is Test {
+    Buzzkill private buzzkill;
 
+    address USER = makeAddr("user");
 
+    function setUp() public {
+        buzzkill = new Buzzkill();
+    }
 
+    function test_NameIsCorrect() public view {
+        string memory expectedName = "Buzzkill";
+        string memory actualName = buzzkill.name();
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    // function test_RevertMintWithoutValue() public {
+    //     vm.expectRevert(MintPriceNotPaid.selector);
+    //     buzzkill.mintTo(address(USER));
+    // }
+
+    function test_MintPricePaid() public {
+        buzzkill.mintTo{value: 0.0073 ether}(USER);
+    }
 }
