fff2204a9b5be4576cb5f93258683cb39192761c
ptaku35 <phoenixtaku9@gmail.com>
2023-12-05 23:24:28 +0700

    Add Hive Vault

--- src/Hive/HiveVaultV1.sol
+++ src/Hive/HiveVaultV1.sol
@@ -12,35 +12,42 @@ import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet
 
 /// @title Hive Vault
 contract HiveVaultV1 is Ownable, Pausable, ReentrancyGuard {
+    using EnumerableSet for EnumerableSet.UintSet;
 
     /* -------------------------------------------------------------------------- */
     /*  State Variables                                                           */
     /* -------------------------------------------------------------------------- */
 
+    /// @notice Rewards emitted per day staked
+    uint256 public rate;
 
-
+    /// @notice Endtime of token rewards
+    uint256 public endTime;
 
     /// @notice Staking token contract address
-    BuzzkillNFT buzzkillNFT;
+    BuzzkillNFT public stakingToken;
 
     /// @notice Rewards token contract address
-    Honey honey;
+    Honey public rewardToken;
 
+    /// @notice Set of staked token Ids by address
+    mapping(address => EnumerableSet.UintSet) internal _depositedIds;
 
+    /// @notice Mapping of timestamps from each staked token id
+    mapping(address => mapping(uint256 => uint256)) public _depositedBlocks;
 
+    /// @notice Mapping of tokenIds to their rate modifier
+    mapping(uint256 => uint256) public _rateModifiers;
 
-
-
-
-
-
-
-
-
-
-
-    constructor(address owner) Ownable(owner) {
-
+    constructor(
+        address initialOwner,
+        address buzzkillNFT,
+        address honey,
+        uint256 initialRewardsRate
+    ) Ownable(initialOwner) {
+        stakingToken = BuzzkillNFT(buzzkillNFT);
+        rewardToken = Honey(honey);
+        rate = initialRewardsRate;        
         _pause();
     }
 
@@ -48,28 +55,29 @@ contract HiveVaultV1 is Ownable, Pausable, ReentrancyGuard {
     /*  Logic Functions                                                           */
     /* -------------------------------------------------------------------------- */
 
+    function deposit(uint256 tokenId) external whenNotPaused {}
 
+    function withdraw(uint256 tokenId) external whenNotPaused {}
 
+    function claim() external whenNotPaused {}
 
+    function earned(address account) external view returns (uint256) {}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function depositsOf(address account) external view returns (uint256) {}
 
     /* -------------------------------------------------------------------------- */
     /*  Owner Functions                                                           */
     /* -------------------------------------------------------------------------- */
+
+    function setRate(uint256 newRate) external onlyOwner {}
+
+    function setEndTime(uint256 newEndTime) external onlyOwner {}
+
+    function setRateModifier(uint256 tokenId, uint256 rateModifier) external onlyOwner {}
+
+    function setNewStakingAddress(address newStakingAddress) external onlyOwner {}
+
+    function setNewRewardTokenAddress(address newRewardToken) external onlyOwner {}
 
     /// @notice Pause the contract.
     function pause() external onlyOwner {
