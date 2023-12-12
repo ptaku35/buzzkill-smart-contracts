cb7e28e9240f8e420b815bd8f17a6290d8cc76f9
ptaku35 <phoenixtaku9@gmail.com>
2023-12-12 23:07:18 +0700

    Add sick ASCI art

--- src/NFT/BuzzkillNFT.sol
+++ src/NFT/BuzzkillNFT.sol
@@ -6,6 +6,17 @@ import {Pausable} from "@openzeppelin-contracts/contracts/utils/Pausable.sol";
 import {VRC725Enumerable} from "@vrc725/contracts/extensions/VRC725Enumerable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+
+// 888888b.   888     888 8888888888P 8888888888P 888    d8P  8888888 888      888      
+// 888  "88b  888     888       d88P        d88P  888   d8P     888   888      888      
+// 888  .88P  888     888      d88P        d88P   888  d8P      888   888      888      
+// 8888888K.  888     888     d88P        d88P    888d88K       888   888      888      
+// 888  "Y88b 888     888    d88P        d88P     8888888b      888   888      888      
+// 888    888 888     888   d88P        d88P      888  Y88b     888   888      888      
+// 888   d88P Y88b. .d88P  d88P        d88P       888   Y88b    888   888      888      
+// 8888888P"   "Y88888P"  d8888888888 d8888888888 888    Y88b 8888888 88888888 88888888 
+
+
 contract BuzzkillNFT is VRC725, VRC725Enumerable, ReentrancyGuard, Pausable {
     /* -------------------------------------------------------------------------- */
     /*  Errors                                                                    */
@@ -29,6 +40,7 @@ contract BuzzkillNFT is VRC725, VRC725Enumerable, ReentrancyGuard, Pausable {
 
     constructor(uint256 _mintPrice) {
         // TODO: Need a modifier here for this and the updateMintPrice function
+        // TODO: Maybe need to consider more strongly about the mint price requirements
         if (_mintPrice > 0.00044 ether) revert MintPriceTooLow();
         if (_mintPrice > 5 ether) revert MintPriceTooHigh();
         __VRC725_init("Buzzkill", "BZK", msg.sender);
