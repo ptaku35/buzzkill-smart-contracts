57d4b1b9f94c1840679df0628cc5e4b03d2085fd
ptaku35 <phoenixtaku9@gmail.com>
2023-12-19 15:05:40 +0700

    remove estimate fee function

--- src/NFT/BuzzkillNFT.sol
+++ src/NFT/BuzzkillNFT.sol
@@ -6,6 +6,8 @@ import {Pausable} from "@openzeppelin-contracts/contracts/utils/Pausable.sol";
 import {VRC725Enumerable} from "@vrc725/contracts/extensions/VRC725Enumerable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+
+
 // 888888b.   888     888 8888888888P 8888888888P 888    d8P  8888888 888      888
 // 888  "88b  888     888       d88P        d88P  888   d8P     888   888      888
 // 888  .88P  888     888      d88P        d88P   888  d8P      888   888      888
@@ -14,6 +16,9 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
 // 888    888 888     888   d88P        d88P      888  Y88b     888   888      888
 // 888   d88P Y88b. .d88P  d88P        d88P       888   Y88b    888   888      888
 // 8888888P"   "Y88888P"  d8888888888 d8888888888 888    Y88b 8888888 88888888 88888888
+
+
+
 
 contract BuzzkillNFT is VRC725, VRC725Enumerable, ReentrancyGuard, Pausable {
     /* -------------------------------------------------------------------------- */
@@ -69,7 +74,6 @@ contract BuzzkillNFT is VRC725, VRC725Enumerable, ReentrancyGuard, Pausable {
         _burn(tokenId);
     }
 
-    // TODO: Add base URI
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://bafybeiayhxazprulpurcaz26y74slp3lfayyeu3n547esianwwpf6ha55e/";
     }
@@ -105,11 +109,6 @@ contract BuzzkillNFT is VRC725, VRC725Enumerable, ReentrancyGuard, Pausable {
     /* -------------------------------------------------------------------------- */
     /*  Required Overrides                                                        */
     /* -------------------------------------------------------------------------- */
-
-    /// @dev Required override from VRC725.
-    function _estimateFee(uint256) internal view override returns (uint256) {
-        return minFee();
-    }
 
     // The following functions are overrides required by Solidity.
 
