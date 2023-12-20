aa38bef9a100422a80b3038d08a5bbca91c7029e
ptaku35 <phoenixtaku9@gmail.com>
2023-12-20 10:34:21 +0700

    Update deploy scripts

--- script/DeployHiveVault.s.sol
+++ script/DeployHiveVault.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.23;
 
 import {Script, console} from "lib/forge-std/src/Script.sol";
-import {HiveVaultV1Basic} from "../src/Hive/HiveVaultV1Basic.sol";
+import {HiveVaultV1} from "../src/Hive/HiveVaultV1.sol";
 
 contract DeployHiveVault is Script {
 
@@ -10,16 +10,21 @@ contract DeployHiveVault is Script {
 
     address buzzkill = 0x1a8987e126B572c3De795180A86fCAb643543f92;
     address honey = 0x9BfbBd6b4d523D832c5D5cB0bE7C33EeD91787f1;
-    uint256 initialRewards = 10;
+    address beeSkills = 0x36bc480435F4C55f59CF49d6D4D9B48ca63bDF36;
 
     function run() external  {
         vm.startBroadcast(deployerPrivateKey);
 
-        HiveVaultV1Basic hive = new HiveVaultV1Basic(
-            msg.sender,
-            buzzkill,
-            honey,
-            initialRewards
+        HiveVaultV1 hive = new HiveVaultV1(
+            msg.sender,     // owner
+            buzzkill,       // staking NFT
+            honey,          // reward token
+            beeSkills,      // traits contract
+            10,             // rate
+            3,              // max queens per hive
+            20,             // max workers per hive
+            1 days,         // epoch time
+            0               // lockup duration
         );
         vm.stopBroadcast();
 
